1574fbf344badd0ec898a2e1bdb584282d6bf110
fchiaro <52402131+fchiaro@users.noreply.github.com>
2020-12-30 13:42:09 +0100

    Tests

--- pymodules/dataset_utils/generator_test.py
+++ pymodules/dataset_utils/generator_test.py
@@ -1,20 +1,34 @@
 from data_loading import CustomDataLoader
 from data_generation import DataGenerator
+from vgg_data_loader import VggDataLoader
+import os
+import numpy as np
+import cv2
 
 
-BATCH_SIZE = 32
+BATCH_SIZE = 4 #32
 MODE = 'training'
-CSV_PATH = ''
-DATASET_ROOT_PATH = ''
-data_loader = CustomDataLoader(mode=MODE, csv_names=None, csv_path=CSV_PATH, dataset_root_path=DATASET_ROOT_PATH, batch_size=BATCH_SIZE)
+CSV_PATH = os.path.abspath('')
+DATASET_ROOT_PATH = os.path.abspath('')
+data_loader = VggDataLoader(mode=MODE, csv_names=None, csv_path=CSV_PATH, dataset_root_path=DATASET_ROOT_PATH, batch_size=BATCH_SIZE)
 
 from preprocessing import CustomPreprocessor
 preprocessor = CustomPreprocessor(desired_shape=(96,96))
-data_generator = DataGenerator(preprocessor=None, data_augmenter=None, output_encoder=None, mode=MODE, data_loader=data_loader, batch_size=BATCH_SIZE)
+data_generator = DataGenerator(preprocessor=preprocessor, data_augmenter=None, output_encoder=None, mode=MODE, data_loader=data_loader, batch_size=BATCH_SIZE)
 
+x_batch, y_batch = data_generator[3]
+for image,label in zip(x_batch, y_batch):
+    image = image.astype(np.uint8)
+    cv2.imshow('image', image)
+    print(f'Label: {label}')
+    cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+"""
 length = len(data_generator)
 print(f'Found {length} batches')
 for i in range(0, length):
+    print(f'Requested batch with index {i}', end='\r')
     item = data_generator.__getitem__(i)
 print('\n')
 data_generator.on_epoch_end()
@@ -23,6 +37,8 @@ data_generator.on_epoch_end()
 length = len(data_generator)
 print(f'Found {length} batches')
 for i in range(0, length):
+    print(f'Requested batch with index {i}', end='\r')
     item = data_generator.__getitem__(i)
 print('\n')
 data_generator.on_epoch_end()
+"""
