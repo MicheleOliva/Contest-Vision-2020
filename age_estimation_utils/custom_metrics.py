a34b7d724850e3489be50e4ad386a3af31f2aba3
fchiaro <52402131+fchiaro@users.noreply.github.com>
2021-01-03 00:03:33 +0100

    Added mae implementation for classification-based model

--- age_estimation_utils/custom_metrics.py
+++ age_estimation_utils/custom_metrics.py
@@ -1,7 +1,18 @@
 import tensorflow as tf
+from dataset_utils.groundtruth_encoding import OrdinalRegressionEncoder
 
 def rounded_mae(y_true, y_pred):
   y_true = tf.round(y_true)
   y_pred = tf.round(y_pred)
   abs_difference = tf.abs(y_pred - y_true)
   return tf.reduce_mean(abs_difference, axis=-1)
+
+def ordinal_rounded_mae(y_true, y_pred):
+  y_true = tf.math.reduce_sum(y_true, axis=-1)
+  print(f'y_true reduce sum: {y_true}')
+  y_pred = tf.math.reduce_sum(y_pred, axis=-1)
+  print(f'y_pred reduce sum: {y_pred}')
+  abs_difference = tf.abs(y_pred - y_true)
+  print(f'abs difference: {abs_difference}')
+  return tf.reduce_mean(abs_difference, axis=-1)
+  
